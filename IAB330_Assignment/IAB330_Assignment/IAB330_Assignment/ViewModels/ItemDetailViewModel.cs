a21ff84d621082553e8d27ec523be37681863c2b
_stevenhua <n10317601@qut.edu.au>
2019-09-04 15:08:20 +1000

    Starter Projecter

---
+++ IAB330_Assignment/IAB330_Assignment/IAB330_Assignment/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using IAB330_Assignment.Models;
+
+namespace IAB330_Assignment.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
