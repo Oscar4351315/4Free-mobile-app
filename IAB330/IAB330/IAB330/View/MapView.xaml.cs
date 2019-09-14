4f04430d48af35e1aabaf60bb7e843a0724ab292
Steven <n10317601@qut.edu.au>
2019-09-14 11:38:11 +1000

    - made it runable - removed extra View model - added varying logo and icon sizes - moved map code-behind to viewmodel

--- IAB330/IAB330/IAB330/View/MapView.xaml.cs
+++ IAB330/IAB330/IAB330/View/MapView.xaml.cs
@@ -4,63 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
-//using Xamarin.Essentials;
+using Xamarin.Essentials;
 using Plugin.Geolocator;
 using Plugin.Permissions.Abstractions;
 using Plugin.Permissions;
 using Xamarin.Forms.PlatformConfiguration;
 
+using IAB330.ViewModel;
+
 namespace IAB330.View
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class MapsPage : ContentPage
+    public partial class MapPage : ContentPage
     {
-        public MapsPage()
+        public MapPage()
         {
             InitializeComponent();
-            OpenMap();
-        }
-
-
-
-
-        async void OpenMap()
-        {
-
-
-            await CrossPermissions.Current.RequestPermissionsAsync(Permission.Location);
-            var status = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Location);
-            if (status != PermissionStatus.Granted)
-            {
-                if (await CrossPermissions.Current.ShouldShowRequestPermissionRationaleAsync(Permission.Location))
-                {
-                    await DisplayAlert("Need location", "Gunna need that location", "Quit"); //call function to open location settings
-                    System.Diagnostics.Process.GetCurrentProcess().CloseMainWindow();
-                }
-
-                //status = await CrossPermissions.Current.RequestPermissionAsync<LocationPermission>();
-            }
-
-            if (status == PermissionStatus.Granted) {
-                var location = CrossGeolocator.Current;
-                if (location.IsGeolocationEnabled && location.IsGeolocationAvailable)
-                {
-                    //grabs the user's lat and lng
-                    var position = await location.GetPositionAsync();
-
-                    //move map to start on user's location
-                    myMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(position.Latitude, position.Longitude),
-                                                     Distance.FromMeters(100)));
-                    myMap.IsShowingUser = true;
-                }
-            }
-
-            
-
+            BindingContext = new MapViewModel();
         }
     }
 }
